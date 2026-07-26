d2a372c246be954d6a593502e63774cf3dbf979f
Дмитро Яблоновський <yablonovskydmutro1@gmail.com>
2026-07-27 02:51:03 +0300

    added chunked import into db

--- import-service/src/import_service/importer/sources/csv_source.py
+++ import-service/src/import_service/importer/sources/csv_source.py
@@ -14,8 +14,32 @@ class CsvSource(DataSource):
             reader = csv.DictReader(f)
             return list(reader)
 
-    def load_groups(self) -> list[dict]: #todo this is hardcoded as fak, need to change ts
+    def load_groups(self) -> list[dict]: #todo до праци, туво є написано по варєтски, цево видов поміняти тре
         return self._read_csv("groups.csv")
 
     def load_students(self) -> list[dict]:
         return self._read_csv("students.csv")
+
+    def load_semesters(self) -> list[dict]:
+        return self._read_csv("semesters.csv")
+
+    def load_teachers(self) -> list[dict]:
+        return self._read_csv("teachers.csv")
+
+    def load_subjects(self) -> list[dict]:
+        return self._read_csv("subjects.csv")
+
+    def load_subject_offerings(self) -> list[dict]:
+        return self._read_csv("subject_offerings.csv")
+
+    def load_enrollments(self) -> list[dict]:
+        return self._read_csv("enrollments.csv")
+
+    def load_class_sessions(self) -> list[dict]:
+        return self._read_csv("class_sessions.csv")
+
+    def load_attendance(self) -> list[dict]:
+        return self._read_csv("attendance.csv")
+
+    def load_grades(self) -> list[dict]:
+        return self._read_csv("grades.csv")
